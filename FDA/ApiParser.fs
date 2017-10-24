404d2096f262ee1019fb01d94397f7439a8e32eb
Anton Schwaighofer <antonsc@microsoft.com>
2017-10-24 21:46:20 +0100

    Some reports on FDA API using type providers

---
+++ FDA/ApiParser.fs
@@ -0,0 +1,106 @@
+﻿module ApiParser
+
+open FSharp.Data
+
+/// A Json parser that reads full FDA event records.
+type FdaFullRecord = JsonProvider<"ExampleApiResponse.json">
+
+/// A Json parser that reads the response to a count query,
+/// with only two fields "term" and "count"
+type FdaCountResponse = JsonProvider<"ExampleCountResponse.json">
+/// Gets the results of querying the API with the given URI,
+/// when we expect a count response.
+let CountResponse (uri: string) = (FdaCountResponse.Load uri).Results
+
+/// A Json parser that reads the response to a time series query,
+/// with only two fields "time" and "count"
+type FdaTimeSeries = JsonProvider<"ExampleTimeSeriesResponse.json">
+/// Gets the results of querying the API with the given URI,
+/// when we expect a time series response.
+let TimeseriesResponse (uri: string) = (FdaTimeSeries.Load uri).Results
+
+/// The access key to the API
+let ApiKey = "Wmp4QrEdcegI9UN0y2sIDQszTiy16thCjYaVOWBt"
+/// The first bits of the API URI, including the API key
+let ApiPrefix = sprintf "https://api.fda.gov/drug/event.json?api_key=%s&" ApiKey
+
+/// Gets the URI to retrieve a full list of FDA even records in a given time range,
+/// limiting to the given number of entries. Note: In the best case,
+/// the API will anyhow only report a maximum of 100 response items.
+let GetFullRecordApi startDate endDate numEntries = 
+    let limit = 
+        match numEntries with
+        | Some i -> sprintf "&limit=%i" i
+        | _ -> ""
+    sprintf 
+        "%ssearch=receivedate:[%s+TO+%s]%s"
+        ApiPrefix startDate endDate limit
+
+/// Gets a list of FDA even records in a given time range,
+/// limiting to the given number of entries. Note: In the best case,
+/// the API will anyhow only report a maximum of 100 response items.
+let GetRecords startDate endDate numEntries = 
+    let fullApi = GetFullRecordApi startDate endDate numEntries
+    (FdaFullRecord.Load fullApi).Results
+
+/// Perform a search and count query on the FDA server.
+/// Records will be restricted to the given condition, then a count of
+/// distinct values of the field given in "countBy" is done.
+/// Returns the entries with top count only, limiting to the given number
+/// of response items.
+let GetCountsOrTimeSeries api keyGetter valueGetter (condition: string) countBy isExact limit =
+    let GetCountsApi condition countBy isExact limit =
+        let exact = if isExact then ".exact" else ""
+        sprintf "%ssearch=%s&count=%s%s&limit=%i" ApiPrefix condition countBy exact limit
+    let condition = condition.Replace(' ', '+')
+    let results =
+        try
+            let fullKey = GetCountsApi condition countBy isExact limit
+            printfn "API: %s" fullKey
+            api fullKey
+        with
+        | ex -> 
+            printfn "Request failed: %s" ex.Message
+            [| |]
+    results
+    |> Seq.map (fun r -> keyGetter r, valueGetter r)
+    |> Seq.toList
+
+/// Perform a search and count query on the FDA server.
+/// Records will be restricted to the given condition, then a count of
+/// distinct values of the field given in "countBy" is done.
+/// Returns the entries with top count only, limiting to the given number
+/// of response items.
+let GetCounts = 
+    GetCountsOrTimeSeries CountResponse (fun r -> r.Term) (fun r -> r.Count)
+
+/// Perform a time series query on the FDA server.
+/// Records will be restricted to the given condition, then a count of
+/// distinct values of the field given in "countBy" is done.
+/// Returns the entries with top count only, limiting to the given number
+/// of response items.
+let GetTimeseries = 
+    GetCountsOrTimeSeries TimeseriesResponse (fun r -> r.Time) (fun r -> r.Count)
+
+/// The field that contains the country in which an event was reported.
+let countryField = "primarysource.reportercountry"
+/// The field that contains the reaction that the patient reported.
+let reactionField = "patient.reaction.reactionmeddrapt"
+/// The field that contains the patient's sex.
+let sexField = "patient.patientsex"
+/// The field that contains the pharmacological class, EPC
+let pharmclassField = "patient.drug.openfda.pharm_class_epc"
+/// The field that contains the date when the report was received.
+let receivedateField = "receivedate"
+
+/// Creates a filter condition for the API, format: field:"value"
+let getFilterCondition field value = sprintf "%s:\"%s\"" field value
+/// Creates a filter condition string to restrict to the given country.
+let getCountryCondition = getFilterCondition countryField
+/// Joins 2 filter condition strings to form a logical AND
+let joinFilterCondition cond1 cond2 = cond1 + "+AND+" + cond2
+/// Joins 2 filter condition strings to form a logical OR
+let orFilterCondition cond1 cond2 = sprintf "(%s)+(%s)" cond1 cond2
+
+/// Gets the year from a date field that is an integer like 20170131.
+let getYearFromInt dateAsInt = dateAsInt.ToString().Substring(0, 4)
---
+++ FDA/Program.fs
@@ -0,0 +1,219 @@
+﻿module Program
+
+open ApiParser
+open System
+open System.IO
+
+/// Writes all strings to the given file.
+let writeAllLines file (lines: string seq) = File.WriteAllLines(file, lines)
+
+/// Prints tuples tab-separated to the given file.
+let tupleToFile file lines =
+    lines
+    |> Seq.map (fun (t1, t2) -> sprintf "%A\t%A" t1 t2)
+    |> writeAllLines file
+
+/// Prints 3-tuples tab-separated to the given file.
+let tuple3ToFile file lines =
+    lines
+    |> Seq.map (fun (t1, t2, t3) -> sprintf "%A\t%A\t%A" t1 t2 t3)
+    |> writeAllLines file
+
+/// Prints 4-tuples tab-separated to the given file.
+let tuple4ToFile file lines =
+    lines
+    |> Seq.map (fun (t1, t2, t3, t4) -> sprintf "%A\t%A\t%A\t%A" t1 t2 t3 t4)
+    |> writeAllLines file
+
+/// Flattens tuples that contains a count lookup into individual entries
+/// (denormalized table)
+let unroll lines =
+    lines
+    |> Seq.collect (fun (country, numEntries, reactionCount) ->
+        reactionCount
+        |> Seq.map (fun (reaction, count) -> country, numEntries, reaction, count)
+    )
+
+/// Some random bits where I played around with the API
+let workingWithRawRecords startDate endDate numEntries =
+    let records = ApiParser.GetRecords startDate endDate numEntries
+    printfn "Got %i records" records.Length
+
+    let missing = "(missing)"
+
+    let GetCountry (r: FdaFullRecord.Result) =
+        match r.Primarysource with
+        | Some country -> 
+            match country.Reportercountry with
+            | Some c -> c
+            | _ -> missing
+        | _ -> missing
+
+    let GetReactions (r: FdaFullRecord.Result) =
+        r.Patient.Reaction
+        |> Seq.map (fun r -> r.Reactionmeddrapt)
+
+    let country = 
+        records
+        |> Seq.map GetCountry
+    
+    let numReportsByCountry = 
+        country 
+        |> Seq.countBy id
+        |> Seq.toList
+
+    numReportsByCountry
+    |> tupleToFile "C:/temp/reportByCountry.tsv"
+
+    let numReactionsByCountry =
+        records
+        |> Seq.map (fun r ->
+            let country = GetCountry r
+            let numReactions = GetReactions r |> Seq.length
+            country, float numReactions
+        )
+
+    let averageReactionsPerCountry =
+        numReactionsByCountry
+        |> Seq.groupBy fst
+        |> Seq.map (fun (country, reports) -> 
+            country, Seq.length reports, Seq.averageBy snd reports
+        )
+
+    averageReactionsPerCountry
+    |> tuple3ToFile "C:/temp/averageReactionsPerCountry.tsv"
+
+[<EntryPoint>]
+let main argv = 
+
+    let numCountries = 10
+
+    // This reports several countries twice: US and UNITED STATES,
+    // DE and GERMANY. Ignore for now, because I would have to create a 
+    // list of country name synonyms.
+
+    let byCountry = 
+        GetCounts "" countryField true numCountries
+        |> Seq.toList
+    byCountry
+    |> tupleToFile "C:/temp/byCountry.tsv"
+
+    /// The TOP 5 reported reactions per country, as a list of
+    /// (country, numReports, (reaction, N))
+    let reactionsByCountry = 
+        byCountry
+        |> Seq.truncate numCountries
+        |> Seq.map (fun (c, numEntries) ->
+            let counts = GetCounts (getCountryCondition c) reactionField true 5
+            c, numEntries, counts
+        )
+
+    reactionsByCountry
+    |> unroll
+    |> tuple4ToFile "c:/temp/topReactionsByCountry.tsv"
+
+    /// The per-country breakdown of patient sex, as a list of
+    /// (country, numReports, (sex, N))
+    let sexByCountry =
+        byCountry
+        |> Seq.truncate numCountries
+        |> Seq.map (fun (c, numReports) ->
+            let counts = GetCounts (getCountryCondition c) sexField false 3
+            c, numReports, counts
+        )
+        |> Seq.toList
+    sexByCountry
+    |> unroll
+    |> tuple4ToFile "c:/temp/sexByCountry.tsv"
+
+    let sexMale = "1"
+    let sexFemale = "2"
+    let sexNone = "0"
+    /// Per country, list the number of reports, the fraction of reports
+    /// with sex reported as female, and the fraction of reports where 
+    /// no sex is present.
+    let sexRatio = 
+        sexByCountry
+        |> Seq.map (fun (country, numReports, counts) ->
+            let sexCounts = counts |> Map.ofSeq
+            let fracFemale = (float sexCounts.[sexFemale]) / (float numReports)
+            let fracNone = (float sexCounts.[sexNone]) / (float numReports)
+            country, numReports, fracFemale, fracNone
+        )
+    sexRatio
+    |> tuple4ToFile "c:/temp/sexRatio.tsv"
+
+
+    // Take the country with max number of reports where the sex ratio is roughly 
+    // balanced (GB) and the country with the largest imbalance (US).
+    // JP has more males, BR is similar to the US
+    // All GB, JP, BR report vastly fewer cases than the US (factor 10 or more)
+    let countries = ["US"; "GB"; "JP"; "BR"]
+    /// Creates a per-country per-sex breakdown, counting the given field
+    /// and returning the top N
+    let countByCountryAndSex countField limit =
+        countries
+        |> Seq.collect (fun country ->
+            [ sexFemale; sexMale ]
+            |> Seq.map (fun sex ->
+                let filter = 
+                    getCountryCondition country
+                    |> joinFilterCondition (getFilterCondition sexField sex)
+                let counts = GetCounts filter countField true limit
+                country, sex, counts
+            )
+        )
+
+    /// A breakdown of most frequent symptoms per sex and country
+    let reactionsByCountryAndSex = countByCountryAndSex reactionField 3
+    reactionsByCountryAndSex
+    |> unroll
+    |> tuple4ToFile "c:/temp/reactionsByCountryAndSex.tsv"
+
+    // The different reactions could be due to different drugs that are 
+    // being described:
+    // Which drug classes are most frequently involved? Again break down by
+    // country and sex
+    let drugclassByCountryAndSex = countByCountryAndSex pharmclassField 3
+    drugclassByCountryAndSex
+    |> unroll
+    |> tuple4ToFile "c:/temp/drugclassByCountryAndSex.tsv"
+
+    // Actually we would like to see a breakdown by country, sex,
+    // reported reaction AND pharmacological class, but that's really
+    // awkward with the limited API.
+
+    // Now get some time series, to see why there is such a spike in reports
+    // around 2010
+
+    /// Number of reports per day
+    let reportsPerDay = 
+        let filter = receivedateField + ":[20040101+TO+20171021]"
+        GetTimeseries filter receivedateField false 100
+        |> Seq.toList
+
+    /// Aggregates a per-day sequence into a per-year sequence
+    let getReportsPerYear reportsPerDay = 
+        reportsPerDay
+        |> Seq.groupBy (fun (date,_) -> getYearFromInt date)
+        |> Seq.map (fun (year, entries) -> 
+            year, entries |> Seq.sumBy snd
+        )
+    reportsPerDay
+    |> getReportsPerYear
+    |> tupleToFile "c:/temp/reportsPerYear.tsv"
+
+    /// Per-day number of reports for US only
+    let reportsPerDayUS = 
+        let filter = 
+            let cond1 = receivedateField + ":[20040101+TO+20171021]"
+            let cond2 = getFilterCondition countryField "US"
+            let cond3 = getFilterCondition countryField "UNITED STATES"
+            joinFilterCondition (orFilterCondition cond2 cond3) cond1
+        GetTimeseries filter receivedateField false 100
+        |> Seq.toList
+    reportsPerDayUS
+    |> getReportsPerYear 
+    |> tupleToFile "c:/temp/reportsPerYearUS.tsv"
+
+    0
